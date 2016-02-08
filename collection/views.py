f3229e2ec6df5f4ae9a44d4867c3b46c5439e455
Bradley Wogsland <bradley@wogsland.org>
2016-02-07 20:55:31 -0600

    through chapter 5...

--- collection/views.py
+++ collection/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    munber = 6
+    return render(request, 'index.html', {
+        'munber': munber,
+        'single': 1,
+        'thing': 'velvet worms',
+    })
