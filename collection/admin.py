0d0a6728bad31d1318e5285865c53de5121a3469
Bradley Wogsland <bradley@wogsland.org>
2016-02-14 00:47:18 -0600

    admin

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from collection.models import Thing
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Thing, ThingAdmin)
